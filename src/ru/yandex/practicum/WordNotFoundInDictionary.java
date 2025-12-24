3328c931fceecfd532e8fbc76257196217c63a22
FedFox <foxyfedor3000@gmail.com>
2025-12-24 22:23:03 +0300

    Убрана ошибка стилей

--- src/ru/yandex/practicum/WordNotFoundInDictionary.java
+++ src/ru/yandex/practicum/WordNotFoundInDictionary.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum;
 
-public class WordNotFoundInDictionary extends Exception{
+public class WordNotFoundInDictionary extends Exception {
 
     public WordNotFoundInDictionary(final String message) {
         super(message);
